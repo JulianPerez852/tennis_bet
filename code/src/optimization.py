18bf00b35f92324b074b513ccbf867f094c67cb5
Alejandro <jorge.moscoso@summa-sci.com>
2024-09-03 22:41:24 -0500

    Create a new way to bet, I organize it with the better prob_win give 1, and the rest, 0.05, It don't earn money yet. I think the forecasting initial might to be changed

--- code/src/optimization.py
+++ code/src/optimization.py
@@ -25,16 +25,56 @@ class Optimization:
         max_loss_percentage     = self.max_loss_percentage
 
         # Constraints: the sum of bets should equal total_money
-        constraints = [{'type': 'eq', 'fun': lambda bets: np.sum(bets) - total_money}]
+        constraints = [{'type': 'eq', 'fun': lambda bets: total_money - np.sum(bets)}]
 
         # Every prob of win greater than 0.6 is a mandatory bet
         # If a prob isn't greater than 0.5 I won't bet a lot of money, but if it has a good SR or EV, I still can bet.
         for i, prob in enumerate(self.prob_win):
-            if prob > 0.6:
-                constraints.append({'type': 'ineq', 'fun': lambda bets, i=i: bets[i] - min_percentage * total_money})
-            elif prob < 0.5:
-                constraints.append({'type': 'ineq', 'fun': lambda bets, i=i: max_loss_percentage * total_money - bets[i]})
+            if prob >= 0.7:
+                constraints.append({'type': 'ineq', 'fun': lambda bets, i=i: 1 - bets[i]})
+            elif prob < 0.7:
+                constraints.append({'type': 'ineq', 'fun': lambda bets, i=i: 0.5 - bets[i]})
                 
+                # constraints.append({'type': 'ineq', 'fun': lambda bets, i=i: bets[i] - 0.05 * total_money})
+            # elif prob > 0.6:
+            #     constraints.append({'type': 'ineq', 'fun': lambda bets, i=i: bets[i] - 0.1 * total_money})
+            # elif prob > 0.7:
+            #     constraints.append({'type': 'ineq', 'fun': lambda bets, i=i: bets[i] - 0.15 * total_money})
+            # elif prob > 0.8:
+            #     constraints.append({'type': 'ineq', 'fun': lambda bets, i=i: bets[i] - 0.2 * total_money})
+            # elif prob > 0.9:
+            #     constraints.append({'type': 'ineq', 'fun': lambda bets, i=i: bets[i] - 0.25 * total_money})
+            # if prob < 0.6:
+            #     constraints.append({'type': 'ineq', 'fun': lambda bets, i=i: 0.015 * total_money - bets[i]})
+            # elif prob < 0.7:
+            #     constraints.append({'type': 'ineq', 'fun': lambda bets, i=i: 0.02 * total_money - bets[i]})
+            # elif prob < 0.8:
+            #     constraints.append({'type': 'ineq', 'fun': lambda bets, i=i: 0.025 * total_money - bets[i]})
+            # elif prob < 0.9:
+            #     constraints.append({'type': 'ineq', 'fun': lambda bets, i=i: 0.03 * total_money - bets[i]})
+            # elif prob < 1:
+            #     constraints.append({'type': 'ineq', 'fun': lambda bets, i=i: 0.035 * total_money - bets[i]})
+            # if sr < -0.1:
+            #     constraints.append({'type': 'ineq', 'fun': lambda bets, i=i: 0.015 * total_money - bets[i]})
+            # elif sr < 0:
+            #     constraints.append({'type': 'ineq', 'fun': lambda bets, i=i: 0.02 * total_money - bets[i]})
+            # elif sr < 0.3:
+            #     constraints.append({'type': 'ineq', 'fun': lambda bets, i=i: 0.025 * total_money - bets[i]})
+            # elif sr < 0.6:
+            #     constraints.append({'type': 'ineq', 'fun': lambda bets, i=i: 0.03 * total_money - bets[i]})
+            # elif sr < 1:
+            #     constraints.append({'type': 'ineq', 'fun': lambda bets, i=i: 0.035 * total_money - bets[i]})
+            # if _ev < 3:
+            #     constraints.append({'type': 'ineq', 'fun': lambda bets, i=i: 0.035 * total_money - bets[i]})
+            # elif _ev < 2.5:
+            #     constraints.append({'type': 'ineq', 'fun': lambda bets, i=i: 0.03 * total_money - bets[i]})
+            # elif _ev < 2:
+            #     constraints.append({'type': 'ineq', 'fun': lambda bets, i=i: 0.025 * total_money - bets[i]})
+            # elif _ev < 1.5:
+            #     constraints.append({'type': 'ineq', 'fun': lambda bets, i=i: 0.02 * total_money - bets[i]})
+            # elif _ev < 1:
+            #     constraints.append({'type': 'ineq', 'fun': lambda bets, i=i: 0.015 * total_money - bets[i]})
+  
         # Bounds: each bet must be non-negative
         bounds = [(0, total_money) for _ in range(len(self.df_risk))]
 
